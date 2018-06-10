0dd0c8bf87c6c85e32c3aa7181dddb0d4fc10351
Raviprakash <raviprakash.kb@lnttechservices.com>
2018-06-10 22:57:46 +0530

    Demo sample for custom Xamarin binding for Java AAR

---
+++ SampleAAR/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SampleAAR.Models;
+
+namespace SampleAAR.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
